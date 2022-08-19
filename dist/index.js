30aaf7a49ccf5d8d43c60ad468116e782e341289
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-08-19 12:31:14 +0530

    learned about interface and union

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
 let id = 5;
-console.log(id);
+let isValid = true;
+let count = [1, 1, 23, 4];
+//tuple array
+let employee;
+employee = [
+    [1, "rohan"],
+    [2, "sid"]
+];
+//union (a variable can have more than one type)
+let pid = 11;
+pid = '11qwe';
+const user1 = {
+    id: 1,
+    name: "rohan"
+};
+console.log(user1);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 let id = 5
+let isValid:boolean = true;
 
-console.log(id)
+let count:number[] = [1,1,23,4]
+
+//tuple array
+let employee:[number,string][];
+
+employee = [
+    [1,"rohan"],
+    [2,"sid"]
+]
+
+//union (a variable can have more than one type)
+let pid:string | number = 11;
+
+pid = '11qwe'
+
+
+//interface
+interface userInterface{
+    id:number,
+    name:string
+}
+
+const user1 : userInterface = {
+    id:1,
+    name:"rohan"
+}
+
+console.log(user1)
